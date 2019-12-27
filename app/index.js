9920c5d2fa0a5b3bcf43939245488467b98534d2
Jan Brecka <jan.brecka@gmail.com>
2019-12-27 22:26:40 +0100

    next iteration

--- app/index.js
+++ app/index.js
@@ -1,137 +1,149 @@
 import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
-import * as util from "../common/utils";
-import { me as appbit } from "appbit";
 import { today } from "user-activity";
 import { HeartRateSensor } from "heart-rate";
 import { BodyPresenceSensor } from "body-presence";
 import { display } from "display";
-import { battery } from "power";
+import { me as appbit } from "appbit";
 
 const endingByCount = (count, ending) =>
-  count === 1
-    ? `${count} ${ending}`
-    : `${count} ${ending}s`
+  count === 1 ? `${count} ${ending}` : `${count} ${ending}s`;
 
-clock.granularity = "minutes";
+const zeroPad = i => (i < 10 ? `0${i}` : i);
 
-const bg = document.getElementById("bg")
+const bg = document.getElementById("bg");
 const clockHours = document.getElementById("clock-hours");
 const clockMinutes = document.getElementById("clock-minutes");
 const infoButton = document.getElementById("info-button");
 const infoLabel = document.getElementById("info-label");
-const batteryIndicator = document.getElementById("battery-indicator");
 
-let screen = 0
-let fns = []
-let hrm
+let screens = [];
+let screenIndex = 0;
+let screenId = "";
+let attachScreen;
+let updateScreen;
+let detachScreen;
 
-if (appbit.permissions.granted("access_heart_rate") && HeartRateSensor) {
-  hrm = new HeartRateSensor({ frequency: 1 }) 
+function attachHeartScreen() {
+  clock.granularity = "minutes";
 }
+
+function detachHeartScreen() {}
 
 if (appbit.permissions.granted("access_activity")) {
-  fns.push((element) => {
-    const update = () => {
-      element.text = endingByCount(today.adjusted.steps, "step")
-    }
-    return ["step", update, update]
-  })
-  fns.push((element) => {
-    const update = () => {
-      element.text = `${(today.adjusted.distance / 1000).toFixed(1)} km`
-    }
-    return ["dist", update, update]
-  })
-}
-
-if (hrm) {
-  let onScreen = false
-  
-  const start = () => {
-    if (onScreen) hrm.start()
-  }
-  const stop = () => {
-    hrm.stop()
-  }
-  
-  if (BodyPresenceSensor) {
-    const body = new BodyPresenceSensor();
-    body.addEventListener("reading", () => {
-      if (body.present) {
-        start()
-      } else {
-        stop()
-      }
-    });
-    body.start();
+  function updateStepsScreen() {
+    infoLabel.text = endingByCount(today.adjusted.steps, "step");
   }
 
-  display.addEventListener("change", () => {
-    display.on ? start() : stop();
-  });
+  function attachStepsScreen() {
+    clock.granularity = "seconds";
+    updateStepsScreen();
+  }
 
-  fns.push((element) => {
-    const onRead = () => {
-      element.text = `${hrm.heartRate}`
-    }
-    return [
-      "heart",
-      () => {
-        onScreen = true
-        hrm.addEventListener("reading", onRead);
-        start();
-      },
-      () => {},
-      () => {
-        onScreen = false
-        hrm.removeEventListener("reading", onRead);
-        stop();
-      }
-    ]
-  })
+  function detachStepsScreen() {}
+
+  function updateDistanceScreen() {
+    infoLabel.text = `${(today.adjusted.distance / 1000).toFixed(1)} km`;
+  }
+
+  function attachDistanceScreen() {
+    clock.granularity = "minutes";
+    updateDistanceScreen();
+  }
+
+  function detachDistanceScreen() {}
+
+  screens.push([
+    "step",
+    attachStepsScreen,
+    updateStepsScreen,
+    detachStepsScreen
+  ]);
+  screens.push([
+    "dist",
+    attachDistanceScreen,
+    updateDistanceScreen,
+    detachDistanceScreen
+  ]);
 }
 
-let screenUpdateHandler
-let screenDetachHandler
+if (appbit.permissions.granted("access_heart_rate") && HeartRateSensor) {
+  const hrm = new HeartRateSensor({ frequency: 1 });
+  const body = BodyPresenceSensor ? new BodyPresenceSensor() : null;
 
-const update = () => screenUpdateHandler && screenUpdateHandler()
-const detach = () => screenDetachHandler && screenDetachHandler()
+  function onHrmRead() {
+    infoLabel.text = `${hrm.heartRate} bpm`;
+  }
 
-infoButton.onclick = (event) => {
-  if (screenDetachHandler) screenDetachHandler()
-  screen = (screen + 1) % fns.length
-  const [id, attach, update, detach] = fns[screen](infoLabel)
-  bg.href = `snoop-bg-${id}.jpg`
-  screenUpdateHandler = update
-  screenDetachHandler = detach
-  attach()
+  function updateHeartScreen() {}
+
+  let heartScreenAttached = false;
+
+  function attachHeartScreen() {
+    if (heartScreenAttached) return;
+    heartScreenAttached = true;
+    clock.granularity = "minutes";
+    infoLabel.text = "";
+    hrm.addEventListener("reading", onHrmRead);
+    hrm.start();
+  }
+
+  function detachHeartScreen() {
+    heartScreenAttached = false;
+    infoLabel.text = "";
+    hrm.removeEventListener("reading", onHrmRead);
+    hrm.stop();
+  }
+
+  screens.push([
+    "heart",
+    attachHeartScreen,
+    updateHeartScreen,
+    detachHeartScreen
+  ]);
 }
 
-clock.ontick = (event) => {
+function updateClock(event) {
   const today = event.date;
-  const hours = today.getHours()
-  
-  clockHours.text = preferences.clockDisplay === "12h"
-    ? util.zeroPad(hours % 12 || 12)
-    : util.zeroPad(hours)
-  clockMinutes.text = util.zeroPad(today.getMinutes())
-  update()
+  const hours = today.getHours();
+
+  clockHours.text =
+    preferences.clockDisplay == "12h"
+      ? zeroPad(hours % 12 || 12)
+      : zeroPad(hours);
+  clockMinutes.text = zeroPad(today.getMinutes());
+
+  updateScreen();
 }
 
-const updateBatteryIndicator = () => {
-  if (battery.charging) 
-    batteryIndicator.style.fill = "#0DE111"
-  else if (battery.chargeLevel > 40)
-    batteryIndicator.style.fill = "#212121"
-  else if (battery.chargeLevel > 20)
-    batteryIndicator.style.fill = "#E1AC0D"
-  else if (battery.chargeLevel > 10)
-    batteryIndicator.style.fill = "#E12824"
-  else
-    batteryIndicator.style.fill = "#671015"
+function changeScreen() {
+  if (detachScreen) detachScreen();
+  screenIndex = (screenIndex + 1) % screens.length;
+  const [id, attach, update, detach] = screens[screenIndex];
+  bg.href = `snoop-bg-${id}.jpg`;
+  screenId = id;
+  attachScreen = attach;
+  updateScreen = update;
+  detachScreen = detach;
+  attachScreen();
 }
 
-updateBatteryIndicator()
-battery.onchange = updateBatteryIndicator
+changeScreen();
+
+display.addEventListener("change", () => {
+  display.on ? attachScreen() : detachScreen();
+});
+
+clock.granularity = "minutes";
+clock.ontick = updateClock;
+
+infoButton.onclick = changeScreen;
+
+if (BodyPresenceSensor) {
+  const body = new BodyPresenceSensor();
+  body.addEventListener("reading", () => {
+    if (screenId == "heart") body.present ? attachScreen() : detachScreen();
+  });
+  body.start();
+}
